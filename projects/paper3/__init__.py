44365fb4dddd1a510f20b6f8131ec5a401080692
dh_fight@163.com <hui117>
2019-09-15 17:31:37 +0800

    paper3 代码

---
+++ projects/paper3/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+# @author: dinghui
+# @file: __init__.py.py
+# @time: 2019/9/12 20:02
+# @desc:
+'''
---
+++ projects/paper5-transformer/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+# @author: dinghui
+# @file: __init__.py.py
+# @time: 2019/9/15 14:37
+# @desc:
+'''
